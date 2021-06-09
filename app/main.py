bd810f56dc770715126acaf99d5201a107a31dd3
benayacp <coolcool.rio75@gmail.com>
2021-06-09 13:29:43 +0000

    improve predict into batch'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-from fastapi import FastApi
-from model import SentenceInput
+from fastapi import FastAPI
+from model import SentenceInput, BatchInput
+import json
 
 app = FastAPI()
 export_dir1 = "model_gru1"
@@ -13,6 +14,24 @@ def predict_two_sentences(input: SentenceInput):
     try:
         similarity = model.predict([np.asarray([input.firstSentence]),np.asarray([input.secondSentence])])
         return {"status_code": 200, "body": f"similarity is {similarity}"}
-    except:
-        return {"status_code": 400, "body": "error"}
+    except Exception as e:
+        return {"status_code": 400, "body": f"error {e}"}
+
+@app.post("/predict_batch")
+def predict_batch(input: BatchInput):
+    threshold = 0.5
+    similar_sentences = []
+    try:
+        for report in input.batch:
+            report_json = json.loads(report.json())
+            report_desc = report_json["deskripsi"]
+            similarity = model.predict([np.asarray([input.firstSentence]), np.asarray([report_desc])])
+            if (similarity[0][0] >= threshold):
+                similar_sentences.append(report_json)
+        if(len(similar_sentences) < 0):
+            return {"status_code": 200, "body": "no similar sentence found"}
+        else:
+            return {"status_code": 200, "body": { "similar_sentences": similar_sentences } }
+    except Exception as e:
+        return {"status_code": 400, "body": f"error {e}" }
 
--- app/model.py
+++ app/model.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+class ReportModel(BaseModel):
+    id_laporan: int
+    kategori: str
+    sub_kategori: str
+    deskripsi: str
+    latitude: float
+    longitude: float
+    foto: str
+    vote: int
+    id_kecamatan: str
+    createdat: datetime
 
 class SentenceInput(BaseModel):
     firstSentence: str
     secondSentence: str
+
+class BatchInput(BaseModel):
+    firstSentence: str
+    batch: List[ReportModel]
+
